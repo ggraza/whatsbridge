c846fad0e60cc5d80e15565bb32703bbae64efd7
frappe <frappe@G2Virtu>
2026-06-02 15:06:22 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## G2 Whats Bridge
+
+WhatsBridge - WhatsApp Integration and Communication Platform for Frappe and ERP
+
+#### License
+
+MIT
---
+++ whatsbridge/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "G2 Whats Bridge",
+			"type": "module",
+			"label": _("G2 Whats Bridge")
+		}
+	]
